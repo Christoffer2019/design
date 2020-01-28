b8ad9eeb3f2eb9ee9d685aa35646ea390bd216f2
Christoffer Lymalm <chly19@student.bth.se>
2020-01-28 13:16:20 +0000

    Added a function in htdocs/js/main.js for changing image path based on the current width of the browser window.

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,8 +1,38 @@
 /**
- * To show off JS works and can be integrated.
+ * Function for images.
  */
-(function() {
+(function () {
     "use strict";
 
-    console.info("main.js ready and loaded.");
-})();
+    var mePresentation = document.getElementById("me_presentation");
+    var about = document.getElementById("about_img");
+
+    changeImageSize();
+
+    window.onresize = changeImageSize;
+
+    function changeImageSize() {
+        /**
+         * Change image size based on the current width of the browser window.
+         */
+        var width = window.innerWidth;
+        var pathStart = "img/kmom03/";
+        var pathEnd = ".jpg";
+
+        if (width >= 700) {
+            if (mePresentation) {
+                mePresentation.src = pathStart + "me-presentation480x539" + pathEnd;
+            }
+            if (about) {
+                about.src = pathStart + "about600x399" + pathEnd;
+            }
+        } else {
+            if (mePresentation) {
+                mePresentation.src = pathStart + "me-presentation1080x1212" + pathEnd;
+            }
+            if (about) {
+                about.src = pathStart + "about1080x719" + pathEnd;
+            }
+        }
+    }
+}());
