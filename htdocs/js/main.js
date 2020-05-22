7493cc1c6af70609cdff352e9bf5067f20eec419
Christoffer Lymalm <chly19@student.bth.se>
2020-05-22 10:45:46 +0100

    Added images to resize based on window width in htdocs/js/main.js

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -6,6 +6,8 @@
 
     var mePresentation = document.getElementById("me_presentation");
     var about = document.getElementById("about_img");
+    var test = document.getElementById("test_img");
+    var rapport = document.getElementById("rapport_img");
 
     changeImageSize();
     window.onresize = changeImageSize;
@@ -16,22 +18,34 @@
          * Change image size based on the current width of the browser window.
          */
         var width = window.innerWidth;
-        var pathStart = "img/kmom03/";
+        var pathStart = "img/kmom06/";
         var pathEnd = ".jpg";
 
         if (width >= 700) {
             if (mePresentation) {
-                mePresentation.src = pathStart + "me-presentation480x539" + pathEnd;
+                mePresentation.src = pathStart + "me-presentation480x640" + pathEnd;
             }
             if (about) {
-                about.src = pathStart + "about600x399" + pathEnd;
+                about.src = pathStart + "about600x400" + pathEnd;
+            }
+            if (test) {
+                test.src = pathStart + "test600x414" + pathEnd;
+            }
+            if (rapport) {
+                rapport.src = pathStart + "rapport600x418" + pathEnd;
             }
         } else {
             if (mePresentation) {
-                mePresentation.src = pathStart + "me-presentation1080x1212" + pathEnd;
+                mePresentation.src = pathStart + "me-presentation1080x1440" + pathEnd;
             }
             if (about) {
-                about.src = pathStart + "about1080x719" + pathEnd;
+                about.src = pathStart + "about1080x720" + pathEnd;
+            }
+            if (test) {
+                test.src = pathStart + "test1080x746" + pathEnd;
+            }
+            if (rapport) {
+                rapport.src = pathStart + "rapport1080x754" + pathEnd;
             }
         }
     }
